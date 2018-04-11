679a0b09a845dbfd23f290c68e05a19f8954e9d5
Sameet Sapra <sameetandpotatoes@gmail.com>
2018-04-11 10:22:04 -0500

    Symlink dotfiles too

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 ############################
-# .make.sh
+# .install.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
-########## Variables
-
 dir=~/dotfiles # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
-files="vimrc vim zshrc z.sh" # list of files/folders to symlink in homedir
+files="gitconfig vimrc config/fish" # list of files/folders to symlink in homedir
 
-##########
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir to backup existing dotfiles first"
@@ -27,36 +24,3 @@ for file in $files; do
 		echo -n "Creating symlink to $file in home directory."
 		ln -s $dir/$file ~/.$file
 done
-
-function install_zsh
-{
-		# Check if zsh was installed already
-		if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-				# Clone my oh-my-zsh repository from GitHub only if it isn't already present
-                rm -rf ~/.oh-my-zsh
-				git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-				# Set the default shell to zsh 
-                echo -n "Setting default shell to zsh. Please enter your password when you are prompted."
-                sudo chsh $USER -s $(which zsh)	# TODO for some reason this doesn't work either
-		else
-				# Figure out if Linux or OS X
-				platform=$(uname);
-				# If Linux, use apt-get
-				if [[ $platform == 'Linux' ]]; then
-						sudo apt install zsh	# TODO don't think this works
-				# If OS X, use brew
-				elif [[ $platform == 'Darwin' ]]; then
-						brew install zsh
-				fi
-				echo -n "Installed zsh!"
-				# Recall this method
-				install_zsh
-		fi
-		echo -n "Copying theme\n"
-		cp ~/dotfiles/spaceship.zsh-theme ~/.oh-my-zsh/themes/
-		echo -n "Copy the below line to set the default shell to zsh\n"
-		echo -n "chsh -s $(which zsh)"
-		echo -n "After setting the font to Meslo LG or Hack, open a new window to see the effects\n"
-}
-
-install_zsh
